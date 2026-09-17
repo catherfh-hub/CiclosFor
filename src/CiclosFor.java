bea69fa9e793a6729547b8e81e01e01f5369d05d
Catherine <catherfh@gmail.com>
2026-09-17 14:14:49 -0600

    Primer ejemplo de ciclo for

---
+++ src/CiclosFor.java
@@ -0,0 +1,23 @@
+public class CiclosFor {
+    static void main() {
+        String nombre = "Feliz Cumpleanos";
+        System.out.println(nombre.charAt(0));
+        System.out.println(nombre.charAt(1));
+        System.out.println(nombre.charAt(2));
+
+        for (int teQuiero = 0; teQuiero < nombre.length(); teQuiero++){
+            System.out.println(nombre.charAt(teQuiero));
+        }
+    }
+}public class Main {
+    public static void main(String[] args) {
+        String nombre = "Feliz Cumpleanos";
+        System.out.println(nombre.charAt(0));
+        System.out.println(nombre.charAt(1));
+        System.out.println(nombre.charAt(2));
+
+        for (int teQuiero = 0; teQuiero < nombre.length(); teQuiero++){
+            System.out.println(nombre.charAt(teQuiero));
+        }
+    }
+}
